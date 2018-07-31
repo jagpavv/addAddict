c5508e9c1f2ffe86eb39bd596a8bfea159c7b7df
Eunjin Kim <jagpavv@gmail.com>
2018-07-31 23:11:42 +0200

    First commit

---
+++ addAddict/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  addAddict
+//
+//  Created by Eunjin on 7/31/18.
+//  Copyright © 2018 Two Berliners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
