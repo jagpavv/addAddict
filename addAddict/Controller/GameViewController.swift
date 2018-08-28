8a9da27330f8a22f095eff06dcbb055128621a5a
Eunjin Kim <jagpavv@gmail.com>
2018-08-28 13:45:28 +0200

    Add instruction buttons

--- addAddict/Controller/GameViewController.swift
+++ addAddict/Controller/GameViewController.swift
@@ -8,7 +8,9 @@ let KBestTimeKey = "BestTime"
 
 class GameViewController: UIViewController {
 
-  @IBOutlet weak var answerLabel: UILabel!
+  @IBOutlet weak var answerBaseView: UIView!
+  @IBOutlet weak var firstGuessBaseView: UIView!
+  @IBOutlet weak var secondGuessBaseView: UIView!
   @IBOutlet weak var cardsBaseView: UIView!
   @IBOutlet weak var levelLabel: UILabel!
   @IBOutlet weak var timerLabel: UILabel!
@@ -34,18 +36,6 @@ class GameViewController: UIViewController {
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     startGame()
-  }
-
-  @IBAction func startPauseBtnTapped(_ sender: UIButton) {
-    if timer?.isValid == true {
-      timer?.invalidate()
-
-      sender.setTitle("▶︎", for: .normal)
-      sender.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-    } else {
-      runTimer()
-      sender.setTitle("||", for: .normal)
-    }
   }
 
   func startGame() {
@@ -78,7 +68,15 @@ class GameViewController: UIViewController {
         answer += ramdomValue
         cards.remove(at: randomIndex)
       }
-      answerLabel.text = String(answer)
+
+      let fa = CGRect(x: 0, y: 0, width: answerBaseView.frame.width, height: answerBaseView.frame.height)
+      let ff = CGRect(x: 0, y: 0, width: firstGuessBaseView.frame.width, height: firstGuessBaseView.frame.height)
+      let answerButton = CardButton(type: .answer, frame: fa, value: answer)
+      let firstGuessButton = CardButton(type: .guess, frame: ff)
+      let secondGuessButton = CardButton(type: .guess, frame: ff)
+      answerBaseView.addSubview(answerButton)
+      firstGuessBaseView.addSubview(firstGuessButton)
+      secondGuessBaseView.addSubview(secondGuessButton)
     } else {
       endGame()
     }
@@ -86,8 +84,6 @@ class GameViewController: UIViewController {
 
   func showCards(animated: Bool = false) {
     guard let game = game else { return }
-
-    cardsBaseView.layer.cornerRadius = cardsBaseView.frame.width / 20
 
     let pad: CGFloat = 10.0
     let totalWidth = cardsBaseView.frame.width
@@ -148,9 +144,7 @@ class GameViewController: UIViewController {
   func moveToNextLevel() {
     level += 1
     randomNumUiform += 5
-
-    levelLabel.text = String("LV. \(level)")
-
+    levelLabel.text = String("Lv \(level)")
 
     // userDefault Level
     let storedLevel = userDefault.integer(forKey: kHightLevelKey)
@@ -168,7 +162,6 @@ class GameViewController: UIViewController {
       userDefault.synchronize()
     }
     startGame()
-    //    }
   }
 
   func endGame() {
@@ -176,18 +169,14 @@ class GameViewController: UIViewController {
   }
 
   func runTimer() {
-    if timer != nil {
-      timer?.invalidate()
-    }
+    if let timer = timer { timer.invalidate() }
     timer = Timer.scheduledTimer(withTimeInterval: 1,
                                  repeats: true,
                                  block: { _ in
                                   self.seconds += 1
-
                                   let min = (self.seconds / 60) % 60
                                   let sec = self.seconds % 60
-
-                                  let minSec = String(format: "Time. %0.2d : %0.2d", min, sec)
+                                  let minSec = String(format: "%0.2d:%0.2d", min, sec)
                                   self.timerLabel.text = minSec
     })
   }
@@ -201,12 +190,12 @@ extension GameViewController: CardButtonDelegate {
       game.cards[button.index].picked = true
       button.isPicked = true
       guess.append(button.value)
+
     } else {
       game.cards[button.index].picked = false
       button.isPicked = false
       guess = guess.filter { $0 != button.value }
     }
-    print("guess: \(guess)")
     moveTo()
   }
 }
--- addAddict/View/CardButton.swift
+++ addAddict/View/CardButton.swift
@@ -4,7 +4,17 @@ protocol CardButtonDelegate: class {
   func cardButtonTapped(button: CardButton)
 }
 
+enum CardType {
+  case card
+  case answer
+  case guess
+}
+
 class CardButton: UIButton {
+  static let unusedIndex = -1
+  static let unusedValue = -1
+
+  let type: CardType
   var value: Int
   var index: Int
   var isPicked: Bool = false {
@@ -16,19 +26,37 @@ class CardButton: UIButton {
   weak var delegate: CardButtonDelegate?
 
   init(index: Int, value: Int, frame: CGRect, delegate: CardButtonDelegate? = nil) {
+    self.type = .card
     self.value = value
     self.index = index
     self.delegate = delegate
     self.isPicked = false
     super.init(frame: frame)
+    setupUI()
+  }
 
-    self.setTitle(String(value), for: .normal)
+  init(type: CardType, frame: CGRect, value: Int = CardButton.unusedValue) {
+    self.type = type
+    self.value = value
+    self.index = CardButton.unusedIndex
+    super.init(frame: frame)
+    setupUI()
+  }
+
+  func setupUI() {
     self.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
     self.backgroundColor = #colorLiteral(red: 1, green: 0.8, blue: 0, alpha: 1)
-    self.titleLabel?.font = UIFont.boldSystemFont(ofSize: 40)
+    self.titleLabel?.font = UIFont.boldSystemFont(ofSize: type == .card ? 40 : 50 )
     self.addTarget(self, action: #selector(cardBtnTapped), for: .touchUpInside)
     self.layer.cornerRadius = self.frame.width / 6
+    self.isUserInteractionEnabled = type == .card
 
+    var title = String(value)
+    if type == .guess, value == CardButton.unusedValue {
+      title = ""
+      self.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+    }
+    self.setTitle(title, for: .normal)
   }
 
   required init?(coder aDecoder: NSCoder) {
@@ -38,5 +66,4 @@ class CardButton: UIButton {
   @objc func cardBtnTapped(sender: UIButton) {
     delegate?.cardButtonTapped(button: self)
   }
-
 }
